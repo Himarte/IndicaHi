446450d9a3c965bd04af93e5df1ebdb2b89ea54d
Othavio Quiliao <othavioaqa@gmail.com>
2024-04-18 18:27:34 -0300

    Agora sim

--- src/routes/dashboard/indicacoes/pendentes/+page.svelte
+++ src/routes/dashboard/indicacoes/pendentes/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 	import DataTable from '../../../../lib/components/ui/data-table/data-table.svelte';
 	import type { PageData } from './$types';
-    
-    export let data: PageData;
-	
+
+	export let data: PageData;
 </script>
 
-<div class="container mx-auto py-10">
-	<DataTable />
-</div>
+<DataTable />
