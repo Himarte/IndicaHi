795232253f7a92752e71d1b302adf8632ceec2cc
Othavio Quiliao <othavioaqa@gmail.com>
2024-05-09 17:59:21 -0300

    API finalizada + Melhorias e otimizações do dashboard

--- src/routes/dashboard/indicacoes/pendentes/+page.svelte
+++ src/routes/dashboard/indicacoes/pendentes/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import DataTable from '$lib/components/ui/data-table-v-externo/data-table.svelte';
+	import { Circle3 } from 'svelte-loading-spinners';
 	import type { PageServerData } from './$types';
+
 	export let data: PageServerData;
-	const leads = data.leads;
 </script>
 
-{#await leads}
-	<p>Carregando...</p>
+{#await data.leads}
+	<div class="flex h-screen w-full items-center justify-center">
+		<Circle3
+			size="70"
+			ballBottomLeft="#F97316"
+			ballBottomRight="#FAFAFA"
+			ballTopLeft="#FAFAFA"
+			ballTopRight="#F97316"
+		/>
+	</div>
 {:then leads}
 	<DataTable {leads} />
 {:catch error}
