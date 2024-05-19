ea9399ac06a3b286d30d786afee4ccc8b68b281b
Othavio Quiliao <othavioquiliao@gmail.com>
2024-05-19 13:19:02 -0300

    Up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,14 +3,9 @@ import type { Config } from 'drizzle-kit';
 
 export default {
 	schema: './src/lib/server/database/schema.ts',
-	out: './migration',
-	driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+	dialect: 'postgresql',
 	dbCredentials: {
-		host: process.env.DB_HOST!,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME!,
-		port: 3333
+		url: process.env.DB_URL!
 	},
 	// Print all statements
 	verbose: true,
