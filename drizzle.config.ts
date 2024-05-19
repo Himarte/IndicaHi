419269f132f00f9aec609786846f15610667b01a
Othavio Quiliao <othavioaqa@gmail.com>
2024-05-19 15:47:41 -0300

    Funcionando Update Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,14 @@
-import 'dotenv/config';
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
 	schema: './src/lib/server/database/schema.ts',
-	out: './migration',
-	driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+	dialect: 'postgresql',
+	out: './drizzle',
 	dbCredentials: {
-		host: process.env.DB_HOST!,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME!,
-		port: 3333
+		url: process.env.DATABASE_URL!
 	},
 	// Print all statements
 	verbose: true,
 	// Always ask for confirmation
 	strict: true
-} satisfies Config;
+});
