7be84d5b2cd3ba621ff69b314237917ffc1b7be1
Othavio Quiliao <othavioaqa@gmail.com>
2024-10-21 18:03:00 -0300

    Server ajustes

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -2,20 +2,27 @@ import type { PageServerLoad } from './$types';
 import { SITE_CHAVE_API } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const leads = fetch('/api/indicacoes', {
-		method: 'GET',
-		headers: {
-			'API-KEY': SITE_CHAVE_API,
-			'Content-Type': 'application/json'
-		}
-	})
-		.then((res) => res.json())
-		.catch((err) => {
-			console.error(err);
-			return [];
+	try {
+		const res = await fetch('/api/indicacoes', {
+			method: 'GET',
+			headers: {
+				'API-KEY': SITE_CHAVE_API,
+				'Content-Type': 'application/json'
+			}
 		});
 
-	return {
-		leads
-	};
+		// Verifica se a resposta é JSON
+		const contentType = res.headers.get('content-type');
+		if (contentType && contentType.includes('application/json')) {
+			const leads = await res.json();
+			return { leads };
+		} else {
+			// Lidar com resposta inesperada (não JSON)
+			console.error('A resposta não é JSON:', await res.text());
+			return { leads: [] };
+		}
+	} catch (err) {
+		console.error('Erro ao buscar leads:', err);
+		return { leads: [] };
+	}
 };
