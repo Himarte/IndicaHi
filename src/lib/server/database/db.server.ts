54c85d4db598295bca07c837bbb594d2bd1d9480
Othavio Quiliao <othavioaqa@gmail.com>
2024-10-21 15:25:31 -0300

    AJustes

--- src/lib/server/database/db.server.ts
+++ src/lib/server/database/db.server.ts
@@ -1,13 +1,11 @@
 // // Aqui fica a conexão com o banco de dados, utilizando o Drizzle ORM
-import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
-import { sql } from '@vercel/postgres';
 import { DB_URL } from '$env/static/private';
 const { Pool } = pg;
 
-const client = new Pool({
+const pool = new Pool({
 	connectionString: DB_URL
 });
 
-await client.connect();
-export const db = drizzle(sql);
+export const db = drizzle({ client: pool });
