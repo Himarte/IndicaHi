9665625bb368a2bd9d03e7e21fa558daf9d15625
Othavio Quiliao <othavioaqa@gmail.com>
2024-12-20 16:47:57 -0300

    Ajustes status

--- src/lib/components/Dropdown-dashboard.svelte
+++ src/lib/components/Dropdown-dashboard.svelte
@@ -1,27 +1,26 @@
 <script lang="ts">
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { CircleEllipsis } from 'lucide-svelte';
 	import { Badge } from './ui/badge';
 </script>
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger asChild let:builder>
-		<Button variant="ghost" builders={[builder]}><CircleEllipsis /></Button>
+		<Button variant="outline" builders={[builder]}>Mudar Status</Button>
 	</DropdownMenu.Trigger>
-	<DropdownMenu.Content class="w-56 border-[0.02rem] border-border">
+	<DropdownMenu.Content class="w-44 border-[0.02rem] border-border">
 		<DropdownMenu.Label>Status</DropdownMenu.Label>
 		<DropdownMenu.Separator />
-		<DropdownMenu.RadioGroup>
-			<DropdownMenu.RadioItem value="sendoAtendido"
+		<DropdownMenu.RadioGroup class="flex w-full flex-col items-start justify-center">
+			<DropdownMenu.RadioItem value="sendoAtendido" class="flex w-full items-center  justify-center"
 				><Badge class="w-28 bg-blue-400 hover:bg-blue-400">Sendo Atendido</Badge
 				></DropdownMenu.RadioItem
 			>
-			<DropdownMenu.RadioItem value="bottom"
+			<DropdownMenu.RadioItem value="bottom" class="flex w-full items-center justify-center"
 				><Badge class="w-28 bg-green-400 hover:bg-green-400">Finalizado</Badge
 				></DropdownMenu.RadioItem
 			>
-			<DropdownMenu.RadioItem value="right"
+			<DropdownMenu.RadioItem value="right" class="flex w-full items-center justify-center"
 				><Badge class="w-28 bg-gray-400 hover:bg-gray-400">Cancelado</Badge></DropdownMenu.RadioItem
 			>
 		</DropdownMenu.RadioGroup>
