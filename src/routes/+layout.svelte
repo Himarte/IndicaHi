7eeb5d4203d5eff7452716d8f98319a9f0367751
Othavio Quiliao <othavioaqa@gmail.com>
2024-12-30 18:41:00 -0300

    Ajustes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 	import { Toaster } from '$lib/components/ui/sonner';
 	import type { LayoutData } from './$types';
 	import type { userDataFromCookies } from '$lib/server/lucia.server';
-	import NovoHeader from '$lib/components/layouts/header/NovoHeader.svelte';
 	import NovoSide from '$lib/components/layouts/NovoSide.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import PrimeiroLogin from '$lib/components/Dialogs/PrimeiroLogin.svelte';
