9b7d8d9a80b9aefcf187329f52a7ea5ea69bef73
Othavio Quiliao <othavioaqa@gmail.com>
2024-12-30 16:22:57 -0300

    Ajustes finais no home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex h-full w-full items-center justify-center">
-	<Button href="/dashboard">Clique aqui</Button>
+<div class="flex h-full w-full flex-col items-center justify-center text-2xl">
+	<span>Bem Vindo ao </span>
+	<span>Sistema de Indicacoes da</span>
+	<span class="font-bold text-orange-500">HIMARTE</span>
 </div>
